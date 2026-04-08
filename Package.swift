009f30cd8525a97152be68b47e56e7aed0cbe811
Finn <fin@delightroom.com>
2026-04-08 20:48:21 +0900

    SPM

---
+++ Package.swift
@@ -0,0 +1,57 @@
+// swift-tools-version:5.7
+// Package.swift for Daro iOS SDK
+
+import PackageDescription
+
+let package = Package(
+    name: "Daro",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(name: "DaroAds", targets: ["DaroAds"]),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git",
+            exact: "12.14.0"
+        ),
+        .package(url: "https://github.com/googleads/googleads-mobile-ios-mediation-meta.git",            exact: "6.20.100"),
+        .package(url: "https://github.com/googleads/googleads-mobile-ios-mediation-pangle.git",          exact: "7.6.600"),
+        .package(url: "https://github.com/googleads/googleads-mobile-ios-mediation-inmobi.git",          exact: "10.8.600"),
+        .package(url: "https://github.com/googleads/googleads-mobile-ios-mediation-dtexchange.git",      exact: "8.3.800"),
+        .package(url: "https://github.com/googleads/googleads-mobile-ios-mediation-chartboost.git",      exact: "9.11.1"),
+        .package(url: "https://github.com/googleads/googleads-mobile-ios-mediation-applovin.git",        exact: "13.4.0"),
+        .package(url: "https://github.com/googleads/googleads-mobile-ios-mediation-ironsource.git",      exact: "8.10.0"),
+        .package(url: "https://github.com/googleads/googleads-mobile-ios-mediation-liftoffmonetize.git", exact: "7.5.300"),
+        .package(url: "https://github.com/googleads/googleads-mobile-ios-mediation-mintegral.git",       exact: "7.7.900"),
+        .package(url: "https://github.com/googleads/googleads-mobile-ios-mediation-moloco.git",          exact: "4.1.0"),
+        .package(url: "https://github.com/googleads/googleads-mobile-ios-mediation-line.git",            exact: "2.9.2025111900"),
+        .package(url: "https://github.com/googleads/googleads-mobile-ios-mediation-unity.git",           exact: "4.16.0"),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Daro",
+            url: "https://github.com/delightroom/daro-ios-sdk/releases/download/1.1.55-alpha/Daro.xcframework.zip",
+            checksum: "88f4a78236dd65883be7392d626e96b724489cd3ecf4e38ec69253276e6af4b0"
+        ),
+        .target(
+            name: "DaroAds",
+            dependencies: [
+                "Daro",
+                .product(name: "GoogleMobileAds",              package: "swift-package-manager-google-mobile-ads"),
+                .product(name: "MetaAdapterTarget",            package: "googleads-mobile-ios-mediation-meta"),
+                .product(name: "PangleAdapterTarget",          package: "googleads-mobile-ios-mediation-pangle"),
+                .product(name: "InMobiAdapterTarget",          package: "googleads-mobile-ios-mediation-inmobi"),
+                .product(name: "DTExchangeAdapterTarget",      package: "googleads-mobile-ios-mediation-dtexchange"),
+                .product(name: "ChartboostAdapterTarget",      package: "googleads-mobile-ios-mediation-chartboost"),
+                .product(name: "AppLovinAdapterTarget",        package: "googleads-mobile-ios-mediation-applovin"),
+                .product(name: "IronSourceAdapterTarget",      package: "googleads-mobile-ios-mediation-ironsource"),
+                .product(name: "LiftoffMonetizeAdapterTarget", package: "googleads-mobile-ios-mediation-liftoffmonetize"),
+                .product(name: "MintegralAdapterTarget",       package: "googleads-mobile-ios-mediation-mintegral"),
+                .product(name: "MolocoAdapterTarget",          package: "googleads-mobile-ios-mediation-moloco"),
+                .product(name: "LineAdapterTarget",            package: "googleads-mobile-ios-mediation-line"),
+                .product(name: "UnityAdapterTarget",           package: "googleads-mobile-ios-mediation-unity"),
+            ],
+            path: "SPM/DaroAds"
+        ),
+    ]
+)
